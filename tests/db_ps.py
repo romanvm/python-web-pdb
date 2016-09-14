52f7def9722b0c7ab4dfa922689c07f1da98ed4f
Roman Miroshnychenko <romanvm@yandex.ua>
2016-09-14 19:21:52 +0300

    Try to fix tests in Travis CI again

--- tests/db_ps.py
+++ tests/db_ps.py
@@ -2,6 +2,13 @@
 # Author: Roman Miroshnychenko aka Roman V.M.
 # E-mail: romanvm@yandex.ua
 
+from __future__ import print_function
+
+try:
+   input = raw_input
+except NameError:
+   pass
+
 from web_pdb import set_trace; set_trace(patch_stdstreams=True)
 foo = input('Enter something: ')
 print('You have entered: ' + foo)
