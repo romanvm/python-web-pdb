fb05340ff52008ce93beab96069bcfa0a411edff
Roman Miroshnychenko <romanvm@yandex.ru>
2016-10-01 15:25:55 +0300

    Disables bottle debug

--- web_pdb/__init__.py
+++ web_pdb/__init__.py
@@ -21,9 +21,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-
-# Todo: don't forget to remove bottle.debug before release
-
 """
 A web-interface for Python's built-in PDB debugger
 """
