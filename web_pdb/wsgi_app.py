598294e2a20124429b7f4e99e13097320cc73ccf
Roman Miroshnychenko <roman1972@gmail.com>
2018-10-09 13:36:42 +0300

    Do not compress web app response less than 100 characters

--- web_pdb/wsgi_app.py
+++ web_pdb/wsgi_app.py
@@ -54,7 +54,8 @@ def compress(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         if ('gzip' in bottle.request.headers.get('Accept-Encoding', '') and
-                isinstance(result, string_type)):
+                isinstance(result, string_type) and
+                len(result) > 100):
             if isinstance(result, unicode):
                 result = result.encode('utf-8')
             tmp_fo = BytesIO()
